e4efd1e10f8b229649225b6b4f9474b3b81d902a
Romain Moudurier <rmouduri@student.42.fr>
2020-12-03 19:56:57 +0100

    [FIX] form '0*'

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: romain <rmouduri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/01 00:18:09 by romain            #+#    #+#             */
-/*   Updated: 2020/12/03 19:22:19 by romain           ###   ########.fr       */
+/*   Updated: 2020/12/03 19:49:15 by romain           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	main(void)
 	int	n1 = 0, n2;
 	char *str = "shdsaoihd";
 
-	n1 = ft_printf("%-*s\n", 32, "abc");
-	n2 = printf("%-*s\n", 32, "abc");
+	n1 = ft_printf("%0*i\n", 7, -54);
+	n2 = printf("%0*i\n", 7, -54);
 	printf("n1 = %d, n2 = %d\n", n1, n2);
 	return (0);
 }
